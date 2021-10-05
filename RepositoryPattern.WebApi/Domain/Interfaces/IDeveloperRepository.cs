6dafd59651f9ff6ab1ff38018628785132a83af3
Hasan Hüseyin GÜNEŞ <71901498+guneshhg@users.noreply.github.com>
2021-10-05 13:12:12 +0300

    first commit

---
+++ RepositoryPattern.WebApi/Domain/Interfaces/IDeveloperRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IDeveloperRepository :IGenericRepository<Developer> 
+    {
+        IEnumerable<Developer> GetPopularDevelopers(int count);
+    }
+}
---
+++ RepositoryPattern.WebApi/WebApi/Controllers/DeveloperController.cs
@@ -0,0 +1,56 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeveloperController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeveloperController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        [HttpGet("{count}")]
+        public IActionResult GetPopularDevelopers(int count)
+        {
+            var popularDevelopers = _unitOfWork.Developers.GetPopularDevelopers(count);
+            return Ok(popularDevelopers);
+
+        }
+
+        [HttpPost]
+        public IActionResult AddDeveloperAndProject()
+        {
+            var developer = new Developer {
+                    Followers=35,
+                    Name="HHG"
+                };
+
+            var project = new Project
+            {
+                Name = "hhgproject"
+            };
+
+            _unitOfWork.Developers.Add(developer);
+            _unitOfWork.Projects.Add(project);
+            _unitOfWork.Save();
+            return Ok();
+
+        }
+
+
+
+
+    }
+}
